7408c435c500fcefcf2b5b88b0bab8985882e19c
Mothilal S <Vh12597_mech22@velhightech.com>
2025-01-28 22:21:50 +0530

    Create replace .java
    The code attempts to replace the first character in the string s1 with a space if that character is an alphabet (either uppercase or lowercase). However, due to an error in the regular expression, it doesn't achieve the intended result.

---
+++ replace .java
@@ -0,0 +1,9 @@
+public class replace 
+{
+    public static void main(String[] args)
+     {
+        String s1="a1b2c3d4@";
+        System.out.println(s1.replaceAll("^[a-Z]"," "));
+    }
+    
+}
