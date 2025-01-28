c165463115731f6b4e52f5c7051e84bede430a49
Mothilal S <Vh12597_mech22@velhightech.com>
2025-01-28 21:10:42 +0530

    Create q1.java
    The code iterates through the string using two nested loops. The outer loop (i) selects the starting index of the substring, and the inner loop (j) selects the ending index. By varying the starting and ending indices, the code extracts and prints all possible substrings of the given string.

---
+++ q1.java
@@ -0,0 +1,14 @@
+class q1
+{
+    public static void main (String []args)
+    {
+        String s1=" sak ";
+        for (int i=0; i< s1.length();i++)
+        {
+            for (int j=i+1;j<= s1.length();j++)
+            {
+                System.out.println(s1.substring(i,j));
+            }
+    }
+    }
+}
