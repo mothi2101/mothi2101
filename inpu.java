8f24ad6d31c1fe6d77b70b5e177866196f62fa43
Mothilal S <Vh12597_mech22@velhightech.com>
2025-01-30 10:18:08 +0530

    Create inpu.java
    The code iterates five times.  In each iteration, it prints the current value of i (which increases from 1 to 5) and the current value of j (which decreases from 5 to 1).

---
+++ inpu.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class input
+{
+public static void main(String[]args)
+{
+    for(int i=1,j=5;i<=5;i++,j--)
+    {
+        System.out.println(i+" "+j);
+    }
+}    
+}
