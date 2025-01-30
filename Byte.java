950e2a893b01075ba3135b1fc1032c57ba9c0d07
Mothilal S <Vh12597_mech22@velhightech.com>
2025-01-30 16:16:15 +0530

    Create Byte.java
    This tells you that a Byte in Java uses 8 bits of memory, and can store integer values from -128 up to 127.

---
+++ Byte.java
@@ -0,0 +1,10 @@
+//import java.util.scanner;
+public class by
+{
+    public static void main(String[]args)
+    {
+        System.out.println(Byte.SIZE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
