1a187b951a035e5476ca204285fd30ec187c0b6f
Mothilal S <Vh12597_mech22@velhightech.com>
2025-01-31 07:03:47 +0530

    Create TORF.java
    In simpler terms, the program asks the user for a number.  Then, it checks: "Is this number less than 15?"  Finally, it prints the answer to that question ("true" or "false").

---
+++ TORF.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TORF
+{
+   public static void main(String[]args) 
+   {
+    Scanner scan =new Scanner(System.in);
+    int a =scan.nextInt();
+    boolean b=(a<15)?true:false;
+    System.out.println(b);
+   }
+}
